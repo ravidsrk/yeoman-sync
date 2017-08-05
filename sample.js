bc388392045d58aee4aba438b40b82498a588477
Ravindra Kumar <ravidsrk@gmail.com>
2017-08-05 19:32:13 +0530

    Use tmp directory from os

--- sample.js
+++ sample.js
@@ -1,22 +1,22 @@
 const yeomanSync = require('./lib/index.js');
 
 let config = {
-	destination: 'androidstarters-java',
-	rename: [{
-		dest: 'gitignore',
-		src: '.gitignore'
-	}, {
-		dest: 'app/gitignore',
-		src: 'app/.gitignore'
-	}],
-	replace: [{
-		regex: 'io.mvpstarter.sample',
-		replacement: '<%= appPackage %>'
-	}],
-	repo: {
-		branchName: 'develop',
-		url: 'https://github.com/androidstarters/android-starter.git'
-	}
-}
+  destination: 'androidstarters-java',
+  rename: [{
+    dest: 'gitignore',
+    src: '.gitignore'
+  }, {
+    dest: 'app/gitignore',
+    src: 'app/.gitignore'
+  }],
+  replace: [{
+    regex: 'io.mvpstarter.sample',
+    replacement: '<%= appPackage %>'
+  }],
+  repo: {
+    branchName: 'develop',
+    url: 'https://github.com/androidstarters/android-starter.git'
+  }
+};
 
 yeomanSync(config);
